1c1a06d8c11db660e324b679d918d354fb52486d
Haierlab <muhammad.08@students.amikom.ac.id>
2019-08-08 18:44:14 +0700

    cart with modal

--- FRONTEND/cart.js
+++ FRONTEND/cart.js
@@ -1,4 +1,4 @@
-
+function cart () {
   var price = $('#totalPrice'),
   quantity = $('#totalQuantity');
 $('.add').click(function() {
@@ -15,3 +15,21 @@ $('.add').click(function() {
   itemQuantity.show();
   itemQuantity.html(parseInt(itemQuantity.html()) + 1)
 })
+
+//get image
+$('.items').on('click', '.add', function (){
+  function getimage() {
+    //title
+    const peritm = $('.card .card-title').html();
+    console.log(peritm);
+    //link source 
+    const img = $('.items img').attr('src');
+    if(img) {
+    $('.images').html(`<img class="float-right img-thumbnail" src="`+img+`" style="width: 85px; height: 85px">`);
+}
+}
+  getimage();
+})
+}
+
+cart();
